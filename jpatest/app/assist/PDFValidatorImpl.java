9edd98cf3aa3968b11e5b83b7fd089a7fb713c07
Miklos Herperger <herpi4@gmail.com>
2016-05-26 15:07:05 +0200

    removing unneccessary comments

--- jpatest/app/assist/PDFValidatorImpl.java
+++ jpatest/app/assist/PDFValidatorImpl.java
@@ -21,31 +21,6 @@ public class PDFValidatorImpl implements PDFValidator {
 	        valid &= data[3]==0x46; // F
 	        valid &= data[4]==0x2D; // -
 
-	        /*if(data[5]==0x31 && data[6]==0x2E && data[7]==0x33) // version is 1.3 ?
-	        {                  
-	            // file terminator
-	            valid &= data[data.length-7]==0x25; // %
-	            valid &= data[data.length-6]==0x25; // %
-	            valid &= data[data.length-5]==0x45; // E
-	            valid &= data[data.length-4]==0x4F; // O
-	            valid &= data[data.length-3]==0x46; // F
-	            valid &= data[data.length-2]==0x20; // SPACE
-	            valid &= data[data.length-1]==0x0A; // EOL
-	            return valid;
-	        }
-
-	        if(data[5]==0x31 && data[6]==0x2E && data[7]==0x34) // version is 1.4 ?
-	        {
-	            // file terminator
-	            valid &= data[data.length-6]==0x25; // %
-	            valid &= data[data.length-5]==0x25; // %
-	            valid &= data[data.length-4]==0x45; // E
-	            valid &= data[data.length-3]==0x4F; // O
-	            valid &= data[data.length-2]==0x46; // F
-	            valid &= data[data.length-1]==0x0A; // EOL
-	            return valid;
-	        }*/
-
 	        return valid;
 		} catch (IOException e) {
 			e.printStackTrace();
